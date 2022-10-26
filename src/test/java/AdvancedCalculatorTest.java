773bc4cb354e7c2802d0384044d7cc0711b4e996
ibrahim <isharif@famcare.app>
2022-10-26 15:53:10 +0300

    Add AdvancedCalculatorTest test suite

--- src/test/java/AdvancedCalculatorTest.java
+++ src/test/java/AdvancedCalculatorTest.java
@@ -2,17 +2,17 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pita.Calculations;
 
-public class SampleTest {
+public class AdvancedCalculatorTest {
 
    @Test (priority = 2)
-    public void testFactorial() {
+    public void testAdvancedFactorial() {
        Calculations calculator= new Calculations();
        Assert.assertEquals(calculator.factorial(5), 120);
    }
    @Test(priority = 1)
-    public void testFactorial2() {
+    public void testAdvancedFactorial2() {
        Calculations calculator= new Calculations();
-       Assert.assertEquals(calculator.factorial(4), 24);
+       Assert.assertEquals(calculator.factorial(4), 23);
    }
 
 }
